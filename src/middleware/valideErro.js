bc4440389dfec41012b8e3fe064650b88366eabe
Jonathan Santos <jonathan01347@gmail.com>
2022-08-25 22:49:07 -0300

    fix(Remoção): Rmovendo arquivos desnecessarios.

--- src/middleware/valideErro.js
+++
@@ -1,3 +0,0 @@
-const valideError = (req, res, next) => {
-
-}
--- src/services/loginServices.js
+++ src/services/loginServices.js
@@ -1,9 +1,7 @@
 const { User } = require('../database/models');
-// const joiValidate = require('../middleware/joiValidate');
 
 const UserServices = {
   Login: async ({ email }) => {
-   // const check = joiValidate(email);
     const result = await User.findOne({ where: { email } });
     if (!result) throw new Error('400|Invalid fields');
     return result;
